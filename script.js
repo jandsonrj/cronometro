e7056fd9daf73215a6e60fc48b722909c0498e2d
JANDSON VITORINO <101944851+jandsonrj@users.noreply.github.com>
2022-08-22 19:11:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const span = document.querySelector('span')
+const btnStart = document.querySelector('#btnStart')
+const btnPause = document.querySelector('#btnPause')
+const btnClean = document.querySelector('#btnClean')
+
+var interval
+
+btnStart.onclick = () => {
+  btnStart.disabled = true
+  btnPause.disabled = false
+
+  var value = parseFloat(span.innerHTML)
+
+  interval = setInterval(() => {
+    value += 0.1
+
+    span.innerHTML = value.toFixed(1)
+  }, 100);
+}
+
+btnPause.onclick = () => {
+  btnStart.disabled = false
+  btnPause.disabled = true
+
+  clearInterval(interval)
+}
+
+btnClean.onclick = () => {
+  btnStart.disabled = false
+  btnPause.disabled = true
+  
+  clearInterval(interval)
+  span.innerHTML = "0.0"
+}
